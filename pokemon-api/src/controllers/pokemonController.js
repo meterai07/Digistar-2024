d259b4621b2cd80710a0fa1ce00e70e373b3c8c1
meterai07 <t.mrr39@gmail.com>
2024-08-29 13:28:56 +0700

    fix: post, put, delete, patch, head, options

--- pokemon-api/src/controllers/pokemonController.js
+++ pokemon-api/src/controllers/pokemonController.js
@@ -24,6 +24,63 @@ class PokemonController {
             response(res, 500, 'An error occurred while retrieving Pokemon', null, error.message);
         }
     }
+
+    async postPokemon(req, res) {
+        const { name, type } = req.body;
+        try {
+            response(res, 201, 'Pokemon created successfully', []);
+        } catch (error) {
+            response(res, 500, 'An error occurred while creating Pokemon', null, error.message);
+        }
+    }
+
+    async updatePokemon(req, res) {
+        const { id } = req.params;
+        const { name, type } = req.body;
+        try {
+            response(res, 200, 'Pokemon updated successfully', []);
+        } catch (error) {
+            response(res, 500, 'An error occurred while updating Pokemon', null, error.message);
+        }
+    }
+
+    async patchPokemon(req, res) {
+        const { id } = req.params;
+        const { name, type } = req.body;
+        try {
+            response(res, 200, 'Pokemon updated successfully', []);
+        } catch (error) {
+            response(res, 500, 'An error occurred while updating Pokemon', null, error.message);
+        }
+    }
+
+    async deletePokemon(req, res) {
+        const { id } = req.params;
+        try {
+            response(res, 200, 'Pokemon deleted successfully', []);
+        } catch (error) {
+            response(res, 500, 'An error occurred while deleting Pokemon', null, error.message);
+        }
+    }
+
+    async headPokemon(req, res) {
+        const { id } = req.params;
+        try {
+            response(res, 200, 'Pokemon header retrieved successfully', []);
+        } catch (error) {
+            response(res, 500, 'An error occurred while retrieving Pokemon header', null, error.message);
+        }
+    }
+
+    async optionsPokemon(req, res) {
+        const { id } = req.params;
+        try {
+            var options = ['GET', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'];
+            response(res, 200, 'Pokemon options retrieved successfully', options);
+        } catch (error) {
+            response(res, 500, 'An error occurred while retrieving Pokemon options', null, error.message);
+        }
+    }
 }
 
 module.exports = PokemonController;
--- pokemon-api/src/routes/pokemonRoutes.js
+++ pokemon-api/src/routes/pokemonRoutes.js
@@ -6,5 +6,11 @@ const pokemonController = new PokemonController();
 
 router.get('/pokemon', (req, res) => pokemonController.getAllPokemon(req, res));
 router.get('/pokemon/:id', (req, res) => pokemonController.getPokemonById(req, res));
+router.post('/pokemon', (req, res) => pokemonController.postPokemon(req, res));
+router.put('/pokemon/:id', (req, res) => pokemonController.updatePokemon(req, res));
+router.patch('/pokemon/:id', (req, res) => pokemonController.patchPokemon(req, res));
+router.delete('/pokemon/:id', (req, res) => pokemonController.deletePokemon(req, res));
+router.head('/pokemon', (req, res) => pokemonController.headPokemon(req, res));
+router.options('/pokemon', (req, res) => pokemonController.optionsPokemon(req, res));
 
 module.exports = router;
